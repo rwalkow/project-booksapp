187dc9873292fe64cc5543ebbb0006dcb6037904
rwalkow <r.walkow@po.edu.pl>
2022-04-10 15:58:59 +0000

    Modify js code

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,6 @@
 {
   'use strict';
-  
+    
   const select = {
     templateOf: {
       bookProduct: '#template-book', 
@@ -9,13 +9,16 @@
       bookList: '.books-list',
     },
   };
-
+  
   const templates = {
     menuProduct: Handlebars.compile(document.querySelector(select.templateOf.bookProduct).innerHTML),
   };
   
-  const makeView = function(){    
+  const bookImg = {
+    bookImage: 'favorite'
+  };
   
+  const makeView = function(){    
     for(let book of dataSource.books) {      
       const thisBook = this;      
       const generatedHTML = templates.menuProduct(book);     
@@ -24,6 +27,26 @@
       bookContainer.appendChild(thisBook.bookParams);  
     }   
   };
-   
+  
+  const favoriteBooks = [];
+  
+  const initActions = function () {
+    const thisBook = this;
+  
+    thisBook.container = document.querySelector(select.containerOf.bookList);
+    thisBook.bookImages = thisBook.container.querySelectorAll('.book a');
+  
+    for(let bookImage of thisBook.bookImages){
+      bookImage.addEventListener('dblclick', function(event){
+        event.preventDefault();
+        bookImage.classList.toggle(bookImg.bookImage);
+        let bookImageId = bookImage.getAttribute('data-id');
+        favoriteBooks.push(bookImageId);
+      });
+    }
+  };
+  
   makeView();
+  initActions();
 }
+  
